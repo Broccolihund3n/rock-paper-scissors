e08f198ac72a1d25ead5f83d5123f61208d478f8
Broccolihund3n <180009074+Broccolihund3n@users.noreply.github.com>
2024-12-15 14:52:29 +0100

    ADD return

--- javascript.js
+++ javascript.js
@@ -3,19 +3,24 @@
 //step 2, to get the computer choice
 let randomNumber = Math.random()*10;
 
+
 function getComputerChoice(){
   if (randomNumber <4){
    console.log("rock");
+   return "Rock";
 
  } else if (randomNumber >3 && randomNumber <7){
     console.log("paper");
+    return "Paper";
 
  } else {
     console.log("scissors");
+    return "Scissors";
     }
 }
 
 getComputerChoice();
+
 
 //step 3, to get the human choice
 function getHumanChoice() {
