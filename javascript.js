0ddd6d2c8908e92b8bcc937f66395548b336bfe3
Broccolihund3n <180009074+Broccolihund3n@users.noreply.github.com>
2025-01-09 19:31:59 +0100

    ADD playRound function

--- javascript.js
+++ javascript.js
@@ -6,29 +6,54 @@ let randomNumber = Math.random()*10;
 
 function getComputerChoice(){
   if (randomNumber <4){
-   console.log("rock");
+   console.log("The computer picked rock");
    return "Rock";
 
  } else if (randomNumber >3 && randomNumber <7){
-    console.log("paper");
+    console.log("The computer picked paper");
     return "Paper";
 
  } else {
-    console.log("scissors");
+    console.log("The computer picked scissors");
     return "Scissors";
     }
 }
 
-getComputerChoice();
 
 
 //step 3, to get the human choice
 function getHumanChoice() {
-let choice = prompt("Rock, paper or scissors?", "Rock");
+let choice = prompt("Rock, paper or scissors?", "rock");
 console.log("You picked "+ choice);
 }
-getHumanChoice();
+
 
 //step 4, declare score variables
 let computerScore = 0;
 let humanScore = 0;
+
+//step 5, logic to play a round
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+//something is wrong here
+function playRound(humanChoice, computerChoice){
+   if (humanChoice === "rock" && computerChoice === "paper"){
+      console.log("You loose! Paper beats Rock");
+
+   } else if (humanChoice === "rock" && computerChoice ==="scissors"){
+      console.log("You win! Scissors beats paper.");
+
+   } else if(humanChoice === "rock" && computerChoice ==="rock"){
+      console.log("It's a tie. Try again!");
+   }
+   else {
+      console.log("It doesn't work")
+   }
+}
+
+
+playRound(humanChoice, computerChoice);
+
+
